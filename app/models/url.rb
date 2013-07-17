d3c85f6658254a7ea29c1b98df38324fb399da2d
dbc-warrior <apprentice@devbootcamp.com>
2013-07-17 12:26:52 -0700

    added model

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,3 +2,6 @@ class Url < ActiveRecord::Base
   # Remember to create a migration!
   validates :long_url, presence: true, format: { with: /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/, on: :create }
 end
+
+test_url = Url.new(:long_url => "htp://something")
+test_url.valid?
