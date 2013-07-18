f7576e346241d7b9e447fa187aea81ebe72218d3
dbc-warrior <apprentice@devbootcamp.com>
2013-07-17 18:29:33 -0700

    finished mostly working

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,21 @@
 get '/' do
+  
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/short' do
+  new_record = Url.create(:long_url => params[:user_url])
+    @short_url = "http://localhost:9393/#{new_record.id}"
+    erb :index
+  # end
+end
+
+get '/:id' do
+  short_url_id = Url.find(params[:id])
+  if short_url_id 
+    redirect "http://#{short_url_id.long_url}"
+  end
+end
+
+# find_or_create_by
